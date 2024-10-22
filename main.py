eba15f29b4451f3e266cf6fc7ebb6534c3d4bc66
revanddd <143707492+revanddd@users.noreply.github.com>
2024-10-22 16:33:25 +0700

    Update and rename main to main.py

--- main.py
+++ main.py
@@ -4,7 +4,17 @@ import math
 # contoh: ada stack dengan anggota 67. maka, fungsi ini akan menghapus 
 # 33 elemen stack paling bawah (karena 67/2 = 33.5, dibulatkan ke bawah = 33).
 def hapusSetengahBawah(stack: Stack) : 
-    pass
+    len_total = stack.getLen()
+    half_size = stack.getLen() // 2
+    temp_stack = Stack()
+    for _ in range(half_size, len_total):
+        temp_stack.push(stack.pop())
+    for _ in range(half_size):
+        stack.pop()
+    while temp_stack.getLen() > 0:
+        stack.push(temp_stack.pop())
+        
+   
 if __name__ == "__main__":
     s = Stack()
     # Isi Stack
