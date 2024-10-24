2bfc25360a327a525eaad9096d52ba7b35b78063
Deodewanto07 <143709679+Deodewanto07@users.noreply.github.com>
2024-10-24 15:57:19 +0700

    Update Jawaban.c

--- Jawaban.c
+++ Jawaban.c
@@ -1,1 +1,23 @@
+no 1
 
+#include <stdio.h>
+
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original Array: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    //proses balik Array
+
+    printf("\nReversed Array: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
