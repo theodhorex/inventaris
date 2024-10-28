929234c2feefdaf72e24e627d0ede550ef7a051a
Deodewanto07 <143709679+Deodewanto07@users.noreply.github.com>
2024-10-28 12:47:30 +0700

    Update Jawaban.c

--- Jawaban.c
+++ Jawaban.c
@@ -1,30 +1,74 @@
-no 1
 
-#include <stdio.h>
+class Queue:
+    def __init__(self):
+        self.data = []
 
+    def enqueue(self, data):
+        self.data.append(data)
 
-int main() {
-    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int size = sizeof(arr) / sizeof(arr[0]);
+    def dequeue(self):
+        if len(self.data) == 0:
+            return None
+        return self.data.pop(0)
 
-    printf("Original Array: ");
-    for (int i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
-    }
+    def front(self):
+        if len(self.data) == 0:
+            return None
+        return self.data[0]
 
-    //proses balik Array
-    for (int i = 0; i < size / 2; i++) {
-        int temp = arr[i];
-        arr[i] = arr[size - 1 - i];
-        arr[size - 1 - i] = temp;
-    }
+    def get_length(self):
+        return len(self.data)
 
-    printf("\nReversed Array: ");
-    for (int i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
-    }
+    def write_all_data(self):
+        print("DATA: ")
+        for i in self.data:
+            print(f"{i}")
 
-    return 0;
-} 
-YANG PIRAMID JO BLM KELAR
+def get_min(queue):
+    if queue.get_length() == 0:
+        return None
+
+    # Initialize min_value to a high number
+    min_value = float('inf')
+    for item in queue.data:
+        if item < min_value:
+            min_value = item
+    return min_value
+
+def pop(queue):
+    if queue.get_length() == 0:
+        return None
+    
+    # Get the last element in the queue without modifying the queue
+    last_index = queue.get_length() - 1
+    last_item = queue.data[last_index]
+
+    # Return the last item
+    return last_item
+
+if __name__ == "__main__":
+    q = Queue()
+    for i in [10, 8, 5, 60, 1, 2, 3, 4, 100]:
+        q.enqueue(i)
+    
+    print("==========")
+    print("Data Awal")
+    q.write_all_data()
+    print("==========")
+    print(f"Data terkecil = {get_min(q)}")
+    print("==========")
+    print("Pembuktian isi queue tidak berubah")
+    print("==========")
+    print("Data Akhir")
+    q.write_all_data()
+    print("==========")
+    print("============")
+    print("Percobaan Pop")
+    print(f"pop - 1 = {pop(q)}")
+    print(f"pop - 2 = {pop(q)}")
+    print(f"pop - 3 = {pop(q)}")
+    print("=============")
+    print("Hasil Akhir: ")
+    q.write_all_data()
+    print("=============")
 
